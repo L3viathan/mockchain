b73014e09262a8c39d6f6e5c555d901572d9c416
L3viathan <git@l3vi.de>
2018-01-28 21:20:48 +0100

    Add requirements.txt, cleanup handler

---
+++ cleanup.py
@@ -0,0 +1,23 @@
+"""A single node to mine blocks."""
+import os
+from time import sleep
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+SPOOL_DIR = "/var/spool/blockchain/"
+
+
+class CleanupHandler(FileSystemEventHandler):
+    """Event handler"""
+    def on_created(self, event):
+        filename = event.src_path
+        sleep(5)
+        os.remove(filename)
+
+
+if __name__ == '__main__':
+    observer = Observer()
+    observer.schedule(CleanupHandler(), SPOOL_DIR)
+    observer.start()
+    while True:
+        sleep(1)
